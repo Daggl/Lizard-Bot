33729acedef54b3c92ab78e6206f12f31ac8b95d
Daggl <mahoep2000@gmail.com>
2026-02-15 07:16:31 +0100

    Welcome bot funktioniert

--- scripts/encode_image.py
+++
@@ -1,5 +0,0 @@
-import base64, sys, os
-p = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'out_test_welcome.png'))
-with open(p, 'rb') as f:
-    b = f.read()
-print(base64.b64encode(b).decode('ascii'))
